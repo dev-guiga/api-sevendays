ce1183ad1e9a98f52004436cf63d997e4125d78a
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2026-01-19 16:00:35 -0300

    :sparkles: add cpf_cnpj gem and update User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  before_validation { self.cpf = CPF.new(cpf).stripped if cpf.present? }
   has_secure_password
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :first_name, :last_name, presence: true
-
+  validates :first_name, :last_name, :address, :city, :state, :neighborhood, :birth_date, presence: true
+  validates :cpf, cpf: { message: "CPF inválido" }, uniqueness: true, presence: true
+  enum :status, { owner: 0, user: 1, public: 2 }, default: :public
 
   def full_name
     "#{first_name} #{last_name}"
