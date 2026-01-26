df125b7ae23bed1e1fbb745107ead3f51969e664
Guilherme Cataneo <guilhermecataneo02@gmail.com>
2026-01-26 09:26:43 -0300

    :recycle: Refactor user creation to support nested address attributes, enhancing user model with address association and validations for improved data integrity.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :rememberable, :recoverable, :database_authenticatable, :validatable, :trackable, :registerable
 
   has_one :diary, dependent: :destroy
+  has_one :address, dependent: :destroy, inverse_of: :user
+  accepts_nested_attributes_for :address
 
   normalizes :email, with: ->(value) { value.downcase.strip }
   validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :first_name, :email, :last_name, :address, :city, :state, :neighborhood, :birth_date, presence: true
+  validates :first_name, :email, :last_name, :birth_date, presence: true
   validates :username, uniqueness: true, presence: true, length: { minimum: 3, maximum: 20 }
   validates :cpf, uniqueness: true, presence: true
+  validates :address, presence: true
   enum :status, { owner: 0, user: 1, standard: 2 }, default: 1
 
   def full_name
